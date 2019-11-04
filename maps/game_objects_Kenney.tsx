14c35e390943b220a58dc78347cffe9b40792740
sjdedes <sjdedes@buffalo.edu>
2019-11-03 22:15:22 -0500

    Created tilesets and level map (Tiled).

---
+++ maps/game_objects_Kenney.tsx
@@ -0,0 +1,127 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.5" name="game_objects" tilewidth="64" tileheight="64" tilecount="37" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="64" height="64" source="../../tankGame/kenney_topdowntanksredux/PNG/Default size/treeBrown_large.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="11.75" y="9" width="42.75" height="44.25"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <image width="8" height="10" source="../../tankGame/kenney_topdowntanksredux/PNG/Default size/treeBrown_leaf.png"/>
+ </tile>
+ <tile id="2">
+  <image width="36" height="36" source="../../tankGame/kenney_topdowntanksredux/PNG/Default size/treeBrown_small.png"/>
+ </tile>
+ <tile id="3">
+  <image width="26" height="22" source="../../tankGame/kenney_topdowntanksredux/PNG/Default size/treeBrown_twigs.png"/>
+ </tile>
+ <tile id="4">
+  <image width="64" height="64" source="../../tankGame/kenney_topdowntanksredux/PNG/Default size/treeGreen_large.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="10.875" y="9.875" width="42.75" height="44.25"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <image width="20" height="28" source="../../tankGame/kenney_topdowntanksredux/PNG/Default size/barrelBlack_side.png"/>
+ </tile>
+ <tile id="6">
+  <image width="24" height="24" source="../../tankGame/kenney_topdowntanksredux/PNG/Default size/barrelBlack_top.png"/>
+ </tile>
+ <tile id="7">
+  <image width="20" height="28" source="../../tankGame/kenney_topdowntanksredux/PNG/Default size/barrelGreen_side.png"/>
+ </tile>
+ <tile id="8">
+  <image width="24" height="24" source="../../tankGame/kenney_topdowntanksredux/PNG/Default size/barrelGreen_top.png"/>
+ </tile>
+ <tile id="9">
+  <image width="20" height="28" source="../../tankGame/kenney_topdowntanksredux/PNG/Default size/barrelRed_side.png"/>
+ </tile>
+ <tile id="10">
+  <image width="24" height="24" source="../../tankGame/kenney_topdowntanksredux/PNG/Default size/barrelRed_top.png"/>
+ </tile>
+ <tile id="11">
+  <image width="20" height="28" source="../../tankGame/kenney_topdowntanksredux/PNG/Default size/barrelRust_side.png"/>
+ </tile>
+ <tile id="12">
+  <image width="24" height="24" source="../../tankGame/kenney_topdowntanksredux/PNG/Default size/barrelRust_top.png"/>
+ </tile>
+ <tile id="13">
+  <image width="28" height="28" source="../../tankGame/kenney_topdowntanksredux/PNG/Default size/barricadeMetal.png"/>
+ </tile>
+ <tile id="14">
+  <image width="28" height="28" source="../../tankGame/kenney_topdowntanksredux/PNG/Default size/barricadeWood.png"/>
+ </tile>
+ <tile id="15">
+  <image width="28" height="28" source="../../tankGame/kenney_topdowntanksredux/PNG/Default size/crateMetal.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="1.75" y="1.25" width="24" height="24.75"/>
+  </objectgroup>
+ </tile>
+ <tile id="16">
+  <image width="28" height="28" source="../../tankGame/kenney_topdowntanksredux/PNG/Default size/crateMetal_side.png"/>
+ </tile>
+ <tile id="17">
+  <image width="28" height="28" source="../../tankGame/kenney_topdowntanksredux/PNG/Default size/crateWood.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="2.75" y="1.125" width="24" height="24.75"/>
+  </objectgroup>
+ </tile>
+ <tile id="18">
+  <image width="28" height="28" source="../../tankGame/kenney_topdowntanksredux/PNG/Default size/crateWood_side.png"/>
+ </tile>
+ <tile id="19">
+  <image width="48" height="16" source="../../tankGame/kenney_topdowntanksredux/PNG/Default size/fenceRed.png"/>
+ </tile>
+ <tile id="20">
+  <image width="52" height="16" source="../../tankGame/kenney_topdowntanksredux/PNG/Default size/fenceYellow.png"/>
+ </tile>
+ <tile id="21">
+  <image width="50" height="50" source="../../tankGame/kenney_topdowntanksredux/PNG/Default size/oilSpill_large.png"/>
+ </tile>
+ <tile id="22">
+  <image width="14" height="14" source="../../tankGame/kenney_topdowntanksredux/PNG/Default size/oilSpill_small.png"/>
+ </tile>
+ <tile id="23">
+  <image width="32" height="22" source="../../tankGame/kenney_topdowntanksredux/PNG/Default size/sandbagBeige.png"/>
+ </tile>
+ <tile id="24">
+  <image source="../../tankGame/kenney_topdowntanksredux/PNG/Default size/sandbagBeige_open.png"/>
+ </tile>
+ <tile id="25">
+  <image width="32" height="22" source="../../tankGame/kenney_topdowntanksredux/PNG/Default size/sandbagBrown.png"/>
+ </tile>
+ <tile id="26">
+  <image width="42" height="28" source="../../tankGame/kenney_topdowntanksredux/PNG/Default size/sandbagBrown_open.png"/>
+ </tile>
+ <tile id="27">
+  <image width="16" height="48" source="../../tankGame/kenney_topdowntanksredux/PNG/Default size/fenceRed - rotate.png"/>
+ </tile>
+ <tile id="28">
+  <image width="16" height="52" source="../../tankGame/kenney_topdowntanksredux/PNG/Default size/fenceYellow - rotate.png"/>
+ </tile>
+ <tile id="29">
+  <image width="28" height="42" source="../../tankGame/kenney_topdowntanksredux/PNG/Default size/sandbagBeige_openy.png"/>
+ </tile>
+ <tile id="30">
+  <image width="22" height="32" source="../../tankGame/kenney_topdowntanksredux/PNG/Default size/sandbagBrown - rotate.png"/>
+ </tile>
+ <tile id="31">
+  <image width="28" height="42" source="../../tankGame/kenney_topdowntanksredux/PNG/Default size/sandbagBrown_open-rotate.png"/>
+ </tile>
+ <tile id="32">
+  <image width="22" height="32" source="../../tankGame/kenney_topdowntanksredux/PNG/Default size/sandbagBeige - rotate.png"/>
+ </tile>
+ <tile id="33">
+  <image width="16" height="28" source="../../tankGame/kenney_topdowntanksredux/PNG/Default size/specialBarrel2_outline.png"/>
+ </tile>
+ <tile id="34">
+  <image width="52" height="60" source="../../tankGame/kenney_topdowntanksredux/PNG/Default size/tank_darkLarge.png"/>
+ </tile>
+ <tile id="35">
+  <image width="42" height="46" source="../../tankGame/kenney_topdowntanksredux/PNG/Default size/tank_blue.png"/>
+ </tile>
+ <tile id="36">
+  <image width="36" height="36" source="../../tankGame/kenney_topdowntanksredux/PNG/Default size/treeGreen_small.png"/>
+ </tile>
+</tileset>
